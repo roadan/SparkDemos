8d79eec35bcfc92ce6e573ea2ead10a992bfb3ba
Yaniv Rodenski <roadan@gmail.com>
2014-12-22 18:00:28 +0200

    combineByKey example
    added an example for calculating the avg movies per actor per movie

--- src/main/scala/Aggregations.scala
+++ src/main/scala/Aggregations.scala
@@ -38,20 +38,31 @@ class Aggregations {
     // ******************************
 
     // creating a list of all the movies names
-    val mNames = movies.map(m => (m(2).toString))
+    val mNames = movies.map(m => m(2).toString)
     val mNamesString = mNames.reduce(_ + ", " + _)
 
     // creating a list of all the movies names per actor
     val actorMovies = actorsAndMovies.reduceByKey(_ + ", " + _)
     actorMovies.foreach(println)
 
-    // creating a list of all the actors and the value 1 for each movie
-    val actorsCount = actorsAndMovies.map(a => (a._1.toString, 1))
-
     // this is not a proper use, since the movies ordered will be added
     // for each partition but hey, what the Marvel universe for you...
     val actorMoviesOrdered = actorsAndMovies.foldByKey(" movies ordered: ")(_ + ", " + _)
     actorMoviesOrdered.foreach(println)
+
+    // creating a list of all the actors and the value 1 for each movie
+    val actorsCount = actorsAndMovies.map(a => (a._1.toString, 1)).reduceByKey(_ + _)
+    actorsCount.foreach(println)
+
+    val actorsMoviesStr = actorsAndMovies.map(a => (a._1.toString, a._2.toString))
+    val moviesToCount = actorsMoviesStr.join(actorsCount).map(m => m._2)
+
+    // calculating the average of movies per actor per movie
+    val avgAccMoviesPerMovie = moviesToCount.combineByKey(
+      (v) => (v, 1),
+      (acc: (Int, Int), v) => (acc._1 + v, acc._2 + 1),
+      (acc1: (Int, Int), acc2: (Int, Int)) => (acc1._1 + acc2._1, acc1._2 + acc2._2)
+    ).map{ case (key, value) => (key, value._1 / value._2.toFloat) }
 
   }
 
