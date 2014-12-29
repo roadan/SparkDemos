fcca5ddc9da66ef29ca5b6e0bff2dc944e10ca64
Yaniv Rodenski <roadan@gmail.com>
2014-12-29 10:33:56 +0200

    Finished Streaming Examples

--- src/main/scala/TwitterStream.scala
+++ src/main/scala/TwitterStream.scala
@@ -1,19 +1,24 @@
+
 import org.apache.spark.SparkConf
+import org.apache.spark.examples.streaming.StreamingExamples
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.StreamingContext._
 import org.apache.spark.SparkContext._
 
 /**
- * Created by roadan on 12/27/14.
+ * This demo shows streaming data from twitter as well as a
+ * reduceByKeyAndWindow operation
  */
 object TwitterStream {
 
   def main(args: Array[String]) {
 
     val sparkConf = new SparkConf().setAppName("StreamingTwitter")
-    sparkConf.setMaster("spark://ec2-54-154-123-43.eu-west-1.compute.amazonaws.com:7077")
-    sparkConf.setExecutorEnv("spark.executor.memory","4G")
+    // sparkConf.setMaster("spark://ec2-54-77-55-253.eu-west-1.compute.amazonaws.com:7077")
+
+    StreamingExamples.setStreamingLogLevels()
 
     // Set the system properties so that Twitter4j library used by twitter stream
     // can use them to generate OAuth credentials
@@ -22,25 +27,40 @@ object TwitterStream {
     System.setProperty("twitter4j.oauth.accessToken", "352922313-7reCJok6u30oZU0ZULEqSRyVcvIbiYJhnMYUl5J8")
     System.setProperty("twitter4j.oauth.accessTokenSecret", "1ovFiMUDrOFUazyRKYVTpsSn4GkZuWxpZKsfkf5YKdUs7")
 
-    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    val ssc = new StreamingContext(sparkConf, Seconds(2))
 
     val tweetStream = TwitterUtils.createStream(ssc, None, args, StorageLevel.MEMORY_ONLY_SER_2 )
 
-    val sparkTweets = tweetStream.filter( twt => twt.getText.toLowerCase.contains("#sdp14") &&
-                                                 twt.getText.toLowerCase.contains("#apachespark") //&&
+    val sparkTweets = tweetStream.map(twt => (twt.getUser.getId, twt.getText.toLowerCase) //&&
+                                                 // twt.getText.toLowerCase.contains("#apachespark") &&
                                                  //twt.getText.toLowerCase.contains("@yrodenski")
     )
 
     // lets transform our DStream to a key-value pier so we can
     // aggregate later
-    val userTweets = sparkTweets.map(t => (t.getUser.getName, t.getText))
+    //val userTweets = sparkTweets.map(t => (t.getUser.getName, t.getText))
 
     // first, lets print the tweets so I can give one of you a book
-    userTweets.foreachRDD(rdd => rdd.collect.foreach(println))
+    sparkTweets.foreachRDD(rdd => {
+
+      rdd.collect.foreach(t=>{
+        println("---------- start tweet ----------")
+        println(t)
+        println("---------- end tweet ----------")
+      })
+
+    })
 
     println("------------------ userTweets is running ---------------------")
     // lets aggregate!
-    //userTweets.reduceByKeyAndWindow((t)=> t.get)
+    val tweetCount = sparkTweets.reduceByKeyAndWindow(_ + " " + _, Seconds(30))
+                                .map( x => (x._2.split("").length, x._1))
+
+
+    tweetCount.foreachRDD(rdd => {
+      println("****************** Start Window ******************")
+      rdd.sortByKey(false).collect.foreach(println)
+    })
 
     ssc.start()
     ssc.awaitTermination()
